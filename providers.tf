c8369424d9b31def2fb75088cd47d1632a555975
Mansipande28 <90886981+Mansipande28@users.noreply.github.com>
2022-03-04 23:22:15 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"
 }
 
-module "s3" {
-    source = "./s3Module"
+#module "s3" {
+ #   source = "./s3Module"
           
-}
+#}
 
 #bucket name should be unique
     #bucket_name = "s32402" 
